025f57bf6c9322834d03e353764815510a45b5b5
Esteban Hernandez <Esteban Hernandez@DESKTOP-GH66L5N>
2021-10-20 16:37:58 -0500

    actualizacion

--- ciclo3/src/main/java/co/edu/bosque/ciclo3/dao/ProductosDAO.java
+++ ciclo3/src/main/java/co/edu/bosque/ciclo3/dao/ProductosDAO.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.edu.bosque.ciclo3.model.Productos;
 
-public interface ProductosDAO extends JpaRepository<Productos, Integer> {
+public interface ProductosDAO extends JpaRepository<Productos, Long> {
 
 }
